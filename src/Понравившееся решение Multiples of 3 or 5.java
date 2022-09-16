c9a795539bd1dfe0edf3d921ab6a56c01806545e
squvering <79298900+squvering@users.noreply.github.com>
2022-09-16 14:05:33 +0300

    Delete Понравившееся решение Multiples of 3 or 5.java

--- src/Понравившееся решение Multiples of 3 or 5.java
+++
@@ -1,9 +0,0 @@
-import java.util.stream.IntStream;
-
-public class Solution {
-  public int solution(int number) {
-    return IntStream.range(0, number)
-                    .filter(n -> (n % 3 == 0) || (n % 5 == 0))
-                    .sum();
-  }
-}
